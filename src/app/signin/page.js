1312a236285b5a2a3ba4bdf4c57e3db18874f0ff
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2025-01-01 18:51:58 +0500

    Update page.js

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,4 +1,4 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import { redirect } from "next/dist/server/api-utils";
 import { auth, signIn } from "../../../auth";
 
